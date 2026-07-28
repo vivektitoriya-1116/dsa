3c9081cdf759b5dfdd2c054c09484d012909bf5a
Vivek titoriya <vivek.25b01011116@abes.ac.in>
2026-07-28 10:53:53 +0530

    .

--- .vscode/oops.cpp
+++ .vscode/oops.cpp
@@ -1,1 +1,17 @@
-#include <iostream>
+#include <iostream> 
+using namespace std;
+class Player{
+    public:
+    std::string name;
+    void roar()
+    {
+        std::cout<<name<<"shouts!"<<std::endl;
+    }
+};
+int main()
+{
+    Player player1;
+    player1.name="John";
+    player1.roar();
+    return 0;
+}
